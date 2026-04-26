5beca085257b430228d0afe9770e4962b6039434
racappuccio <rocioantonellacappuccio@gmail.com>
2026-04-26 20:45:51 -0300

    Corrección de clase Arquero

--- Arquero.py
+++ Arquero.py
@@ -1,9 +1,5 @@
 from Jugador import Jugador
 
-class JugadorCampo(Jugador):
-    def __init__(self, numero, apellido, posicion, minutos, goles):
-        super().__init__(numero, apellido, posicion, minutos)
-        self.goles = goles
-
-    def mostrar_datos(self):
-        return super().mostrar_datos() + f" - Goles: {self.goles}"
+class Arquero(Jugador):
+    def __init__(self, numero, apellido, minutos):
+        super().__init__(numero, apellido, "Arquero", minutos)
